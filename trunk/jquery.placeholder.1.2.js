fe32a6fac51ac217f476042c5eb2a3d959d59ad8
byf1987@gmail.com <byf1987@gmail.com@125fa6f4-555d-d18e-05ea-7bc46133d705>
2011-09-06 03:39:09 +0000

    Bugfix. Thanks to Tony (ty*****_at_gmail.com)

--- trunk/jquery.placeholder.1.2.js
+++ trunk/jquery.placeholder.1.2.js
@@ -8,10 +8,11 @@
  * Date: Sept 2011
  * Author: Otacon (byf1987_at_gmail.com)
  * Project page: https://code.google.com/p/jquery-placeholder-js/
- * Version: 1.1
+ * Version: 1.2
  * Changelog: 
- 	1.0 Initial release
-	1.1	Updated the code to meet jQuery plugin format. Enabled parameterized init.
+    1.2 Added semicolons to the end of function, thanks to Tony (ty*****_at_gmail.com)
+	1.1	Updated the code to meet jQuery plugin format. Added parameterized init. 
+ 	1.0 Initial release.
  * Tested on: Chrome 10.0; IE6 (IETester); IE8 (IETester)
  * Known Issues: 
  * 	Placeholder for Password is currently not supported
@@ -34,61 +35,61 @@
 			// -- Util Methods --	
 			var getContent = function(element){
 				return $(element).val();		
-			}
+			};
 		
 			var setContent = function(element, content){
 				$(element).val(content);		
-			}
+			};
 			
 			var getPlaceholder = function(element){
 				return $(element).attr("placeholder");
-			}
+			};
 			
 			var isContentEmpty = function(element){
 				var content = getContent(element);
 				return (content.length === 0) || content == getPlaceholder(element);
-			}
+			};
 				
 			var setPlaceholderStyle = function(element){
 				$(element).data($.Placeholder.settings.dataName, $(element).css("color"));
 				$(element).css("color", $.Placeholder.settings.color);		
-			}
+			};
 			
 			var clearPlaceholderStyle = function(element){
 				$(element).css("color", $(element).data($.Placeholder.settings.dataName));		
 				$(element).removeData($.Placeholder.settings.dataName);
-			}
+			};
 			
 			var showPlaceholder = function(element){
 				setContent(element, getPlaceholder(element));
 				setPlaceholderStyle(element);	
-			}
+			};
 			
 			var hidePlaceholder = function(element){
 				if($(element).data($.Placeholder.settings.dataName)){
 					setContent(element, "");
 					clearPlaceholderStyle(element);
 				}
-			}
+			};
 			
 			// -- Event Handlers --
 			var inputFocused = function(){
 				if(isContentEmpty(this)){
 					hidePlaceholder(this);		
 				}
-			}
+			};
 			
 			var inputBlurred = function(){
 				if(isContentEmpty(this)){
 					showPlaceholder(this);
 				}
-			}
+			};
 			
 			var parentFormSubmitted = function(){
 				if(isContentEmpty(this)){
 					hidePlaceholder(this);		
 				}	
-			}
+			};
 			
 			// -- Execution --
 			$("textarea, input[type='text']").each(function(index, element){
